e41dc2f81d8d30e68a9a2102f1561edd71c6c205
Santiago Godoy <santygodoy64@gmail.com>
2025-10-03 17:09:13 -0300

    [FIX]: se actualizaron los changelog para la creacion del estado Create en Trip.

--- src/main/resources/changelog/changes/db.changelog-07.sql
+++ src/main/resources/changelog/changes/db.changelog-07.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 
---changeset AgustinAnil:insercion_valor_state
-insert into state (name, scope) values ('CREATE', 'Trip')
+--changeset SantiagoGodoy:insercion_valor_state
+insert into state (name, scope) values ('CREATE', 'TRIP')
--- src/main/resources/changelog/changes/db.changelog-09.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
-
---changeset SantiagoGodoy:actuaizar_la_tabla_Driver_anadiendo_rating
-ALTER TABLE public.driver ADD COLUMN rating DOUBLE PRECISION;
