6bfb1d1e13f36a2cdf196f18a73faeac7c539517
Santiago Godoy <santygodoy64@gmail.com>
2025-10-03 17:09:13 -0300

    [FIX]: se actualizaron los changelog para la creacion del estado Create en Trip.

--- src/main/resources/changelog/changes/db.changelog-08.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
-
---changeset SantiagoGodoy:actualizar_el_valor_de_Trip_a_TRIP_segun_el_changelog. 
-UPDATE state SET scope = 'TRIP' WHERE name = 'CREATE' AND scope = 'Trip';
