f84f900d397d672ccc8be0c1f4e5e6f66c86d87e
AnilAgustin <anil.agustin06@gmail.com>
2025-11-15 17:45:29 -0300

    [FIX] Cambio de tipo boolean en la peticion filter

--- src/main/java/com/carpool/carpool/controller/reservation/ReservationController.java
+++ src/main/java/com/carpool/carpool/controller/reservation/ReservationController.java
@@ -34,7 +34,7 @@ public class ReservationController {
     @GetMapping("/filter")
     public ResponseEntity<Response<ReservationResponseDTO>> getReservations(@RequestParam(required = false) @Positive(message = "El id del viaje debe ser mayor a 0") Long idTrip,
                                                                             @RequestParam(required = false) Long idStartCity, @RequestParam(required = false) Long idDestinationCity,
-                                                                            @RequestParam(required = false) Boolean baggage, @RequestParam(required = false) String nameState){
+                                                                            @RequestParam(required = false) boolean baggage, @RequestParam(required = false) String nameState){
         return new ResponseEntity<>(reservationService.getReservation(idTrip, idStartCity, idDestinationCity, baggage, nameState), HttpStatus.OK);
     }
 
