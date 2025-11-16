4591ae5014715082718e71cfab5171eb2cd8bb0d
gonzalo-baldassi <gonzalobaldassi02@gmail.com>
2025-11-16 20:41:39 -0300

    [FIX]: Descomentar el urlImagesUsers en ReservationImplementation y mapper el urlImagesUsers

--- src/main/java/com/carpool/carpool/service/reservation/ReservationImplementation.java
+++ src/main/java/com/carpool/carpool/service/reservation/ReservationImplementation.java
@@ -72,15 +72,16 @@ public class ReservationImplementation implements IReservationService{
             return ResponseUtils.buildOKResponse(List.of("No existen reservas para el viaje correspondiente"), null);
         }
         this.LOGGER.info("PASA EL IF");
-//        Map<Long, String> urlImagesUsers = reservations.stream()
-//                .collect(Collectors.toMap(
-//                    reservation -> reservation.getUser().getId(),
-//                        reservation -> mediaService.getProfilePictureUrlByUserId(reservation.getUser().getId())
-//                ));
-//
-//        this.LOGGER.info("IMAGENES {}", urlImagesUsers);
 
-        List<ReservationDTO> listReservation = reservationMapper.convertReservationToReservationDTO(reservations, null);
+        Map<Long, String> urlImagesUsers = reservations.stream()
+                .collect(Collectors.toMap(
+                        r -> r.getUser().getId(),
+                        r -> mediaService.getProfilePictureUrlByUserId(r.getUser().getId()),
+                        (existing, replacement) -> existing
+                ));
+        this.LOGGER.info("IMAGENES {}", urlImagesUsers);
+
+        List<ReservationDTO> listReservation = reservationMapper.convertReservationToReservationDTO(reservations, urlImagesUsers);
         ReservationResponseDTO responseReservation = new ReservationResponseDTO();
         this.LOGGER.info("response RESERVAS {}", responseReservation);
         responseReservation.setReservation(listReservation);
