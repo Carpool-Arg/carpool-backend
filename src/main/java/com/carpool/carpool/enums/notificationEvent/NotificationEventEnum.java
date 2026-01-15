bd9e2b1b2e2b80ee0541cd99b828e241e846a9ae
Santiago Godoy <santygodoy64@gmail.com>
2026-01-14 21:40:44 -0300

    [FIX]: Corrección  en el enum de las notificaciones que agrega el nuevo enum al JAVADOC

--- src/main/java/com/carpool/carpool/enums/notificationEvent/NotificationEventEnum.java
+++ src/main/java/com/carpool/carpool/enums/notificationEvent/NotificationEventEnum.java
@@ -4,6 +4,7 @@ package com.carpool.carpool.enums.notificationEvent;
  * Clase que contiene las diferentes notificaciones que la aplicación puede realizar
  * <p>
  *     {@code RESERVATION_CREATED} Notificación para cuando se crea una reserva
+ *     {@code TRIP_CLOSED_AUTOMATICALLY} Notificación para cuando un viaje se cierra automáticamente
  * </p>
  */
 public enum NotificationEventEnum {
