b5fa68fd2260724cdc956fcd54e266ae00adfafd
Agustin <anil.agustin06@gmail.com>
2026-01-26 17:57:41 -0300

    Srg 123 feat/trip closed full capacity (#51)
    * [DOC]: Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * [FIX]: Correccion del gitignore para el app. properties
    
    * Stop tracking application.properties
    
    * [FEAT]: Implementación de cierre de viaje automático por tiempo límite y envío de notificaciones
    
    * [FIX]: Correcciones en tiempo en la task y carga de datos del feed y Busqueda de viaje
    
    * [FIX]: Corrección  en el enum de las notificaciones que agrega el nuevo enum al JAVADOC
    
    * [FEAT] Implementacion para cerrar viaje cuando alcanza el cupo maximo y enviar notificacion al chofer
    
    * [FEAT] Cambios realizados en db.changelog
    
    * [FIX] Renombramiento de clase que inicia con minusculas
    
    * [FIX] Estado closed y envio de notificacion luego de validar existencia de estado
    
    * [FIX] Seteo de fecha de finalizacion al estado del viaje
    
    * [FEAT]: Implementacion con un utils para cambiar los estados
    
    * [FIX] Incorporacion de enum para viajes y reservas, se incorporo el atributo finish para los estados y se adapto el update de reservas con el utils creado de cambio de estados
    
    * [FIX] Se agrego java doc a clases enum
    
    ---------
    
    Co-authored-by: Gonzalo Baldassi <gonzalobaldassi02@gmail.com>
    Co-authored-by: RoberSalera <robertosalera15@gmail.com>
    Co-authored-by: Santiago Godoy <santygodoy64@gmail.com>

---
+++ src/main/java/com/carpool/carpool/enums/trip/TripStateEnum.java
@@ -0,0 +1,12 @@
+package com.carpool.carpool.enums.trip;
+
+/**
+ * Esta clase contiene los estados que puede poseer una Trip
+ */
+public enum TripStateEnum {
+	CREATED,
+	CLOSED,
+	IN_PROGRESS,
+	CANCELLED, 
+	FINISHED;
+}
