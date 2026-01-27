85758503b4a1366df740209404425f665f8e86d0
Santiago Godoy <santygodoy64@gmail.com>
2026-01-27 19:33:03 -0300

    [FIX]: Correcciones en el mensaje de  inicio de viaje.

--- src/main/java/com/carpool/carpool/service/notification/content/TripStartedNotificationImplementation.java
+++ src/main/java/com/carpool/carpool/service/notification/content/TripStartedNotificationImplementation.java
@@ -6,7 +6,12 @@ import com.carpool.carpool.dto.notificationPayload.NotificationPayloadDTO;
 import com.carpool.carpool.enums.dispatchPolicy.DispatchPolicyEnum;
 import com.carpool.carpool.enums.notificationEvent.NotificationEventEnum;
 import com.carpool.carpool.model.reservation.Reservation;
+import com.carpool.carpool.model.trip.tripStop.TripStop;
+
 import static com.carpool.carpool.utils.EmailMessageUtils.*;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Component
 public class TripStartedNotificationImplementation implements INotificationContentService<Reservation> {
@@ -20,18 +25,21 @@ public class TripStartedNotificationImplementation implements INotificationConte
         return DispatchPolicyEnum.PUSH_THEN_EMAIL;
     }
 
-    @Override
+   @Override
     public NotificationPayloadDTO build(Reservation res) {
         String driver = res.getTrip().getVehicle().getDriver().getUser().getName();
-        String origin = res.getStartCity().getCity().getName();
-        String destination = res.getDestinationCity().getCity().getName();
+        List<TripStop> stops = res.getTrip().getTripStops().stream()
+                .sorted(Comparator.comparingInt(TripStop::getStopOrder))
+                .toList();
+        String tripOrigin = stops.get(0).getCity().getName();
+        String tripDestination = stops.get(stops.size() - 1).getCity().getName();
 
         return NotificationPayloadDTO.builder()
                 .pushTitle(PUSH_TITLE_TRIP_STARTED)
-                .pushBody(String.format(PUSH_BODY_TRIP_STARTED, driver, origin))
+                .pushBody(String.format(PUSH_BODY_TRIP_STARTED, driver, tripOrigin))
                 .emailSubject(SUBJECT_TRIP_STARTED)
                 .emailTitle(String.format(TITLE_TRIP_STARTED, res.getUser().getName()))
-                .emailMessage(String.format(MESSAGE_TRIP_STARTED, driver, origin, destination))
+                .emailMessage(String.format(MESSAGE_TRIP_STARTED, driver, tripOrigin, tripDestination))
                 .emailMessageFooter(FOOTER_TRIP_STARTED)
                 .build();
     }
