22b04fc3bc98279e87ffc13c6406c6283c88d1ad
Roberto Salera <robertosalera15@gmail.com>
2026-02-07 14:42:19 -0300

    [FEAT]: Avance de logica para obtener las reseñas que le han realizado a un chofer

---
+++ src/main/java/com/carpool/carpool/mappers/review/ReviewMapper.java
@@ -0,0 +1,25 @@
+package com.carpool.carpool.mappers.review;
+
+import org.springframework.stereotype.Component;
+
+import com.carpool.carpool.dto.review.DriverReviewResponseDTO;
+import com.carpool.carpool.model.review.Review;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewMapper {
+
+
+  public DriverReviewResponseDTO convertReviewToDriverReviewResponseDTO(Review review){
+    DriverReviewResponseDTO driverReviewResponseDTO = DriverReviewResponseDTO.builder()
+      .reviewId(review.getId())
+      .stars(review.getStars())
+      .createdAt(review.getCreated_at())
+      .description(review.getDescription())
+      .build();
+
+    return driverReviewResponseDTO;
+  }
+}
