e2639178bc320ea8c822c6c90b4434e137df420e
AnilAgustin <anil.agustin06@gmail.com>
2026-02-11 18:55:06 -0300

    [FIX] Resolucion de conflictos en Email

--- src/main/java/com/carpool/carpool/service/notification/dispatch/PushThenEmailPolicyImplementation.java
+++ src/main/java/com/carpool/carpool/service/notification/dispatch/PushThenEmailPolicyImplementation.java
@@ -9,14 +9,8 @@ import com.carpool.carpool.model.user.token.UserToken;
 import com.carpool.carpool.repository.user.token.UserTokenRepository;
 import com.carpool.carpool.service.email.IEmailService;
 import com.carpool.carpool.service.firebase.notification.IFirebaseNotificationService;
-import com.carpool.carpool.service.notification.NotificationImplementation;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-=======
 import lombok.extern.slf4j.Slf4j;
->>>>>>> 5921a09fc7fa9cfdc6a103b560add5e267df7b64
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,35 +22,12 @@ public class PushThenEmailPolicyImplementation implements INotificationDispatchP
     private final UserTokenRepository tokenRepository;
     private final IFirebaseNotificationService firebaseService;
     private final IEmailService emailService;
-    private static final Logger logger = LoggerFactory.getLogger(PushThenEmailPolicyImplementation.class);
 
     @Override
-<<<<<<< HEAD
-    public void execute(User user, NotificationPayloadDTO payload) {
-        List<UserToken> tokens = tokenRepository.findByUserAndTypeAndState(
-                user, TokenTypeEnum.PUSH_NOTIFICATION, TokenStateEnum.ACTIVE
-        );
-
-        logger.info("Policy - Push Then Email. Tokens={}", tokens);
-
-        if (!tokens.isEmpty()) {
-            firebaseService.sendPushNotification(tokens, payload.getPushTitle(), payload.getPushBody());
-        } else {
-            emailService.sendEmail(
-                    user.getEmail(),
-                    payload.getEmailSubject(),
-                    payload.getEmailTitle(),
-                    payload.getEmailMessage(),
-                    payload.getEmailOptionalMessage(),
-                    payload.getEmailButtonUrl(),
-                    payload.getEmailButtonText(),
-                    payload.getEmailMessageFooter()
-=======
     public boolean execute(User user, NotificationPayloadDTO payload) {
         try {
             List<UserToken> tokens = tokenRepository.findByUserAndTypeAndState(
                     user, TokenTypeEnum.PUSH_NOTIFICATION, TokenStateEnum.ACTIVE
->>>>>>> 5921a09fc7fa9cfdc6a103b560add5e267df7b64
             );
 
             if (!tokens.isEmpty()) {
