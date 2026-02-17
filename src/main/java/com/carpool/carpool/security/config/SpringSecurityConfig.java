42c7cc04e7e932c67e51057bdcf7504dff4381c2
Santiago Godoy <santygodoy64@gmail.com>
2026-02-17 12:26:31 -0300

    [FIX]: Corrección al Recaptcha del backend

--- src/main/java/com/carpool/carpool/security/config/SpringSecurityConfig.java
+++ src/main/java/com/carpool/carpool/security/config/SpringSecurityConfig.java
@@ -88,7 +88,7 @@ public class SpringSecurityConfig {
         .exceptionHandling(config -> config
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
         )
-        //.addFilterBefore(new RecaptchaFilter(authRecaptchaService), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(new RecaptchaFilter(authRecaptchaService), UsernamePasswordAuthenticationFilter.class)
         .addFilter(new JwtAuthenticationFilter(authenticationManager(),userRepository, userAccountService, emailImplementation,
                 supportEmail, userTokenRepository, urlUnlockAccount))
         .addFilter(new JwtValidationFilter(authenticationManager(), authBlacklistService, userRepository))
