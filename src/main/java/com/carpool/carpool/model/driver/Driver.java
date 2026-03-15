04935bcfcae5630edd14527cd5058d8f44cb03b2
Roberto Salera <robertosalera15@gmail.com>
2026-03-15 11:31:32 -0300

    [FIX]: Implementacion de Serializable en la clase Driver

--- src/main/java/com/carpool/carpool/model/driver/Driver.java
+++ src/main/java/com/carpool/carpool/model/driver/Driver.java
@@ -1,5 +1,6 @@
 package com.carpool.carpool.model.driver;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.carpool.carpool.model.licenseClass.LicenseClass;
@@ -19,7 +20,7 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name = "driver")
-public class Driver {
+public class Driver implements Serializable{
 
     @Id
     @Column(name = "id")
