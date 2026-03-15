f88a7d35138bb9dc4246e25dadc1a2887f9c1372
gonzabaldassi <gonzalobaldassi02@gmail.com>
2026-03-15 14:51:57 -0300

    [FIX]:  Agregar los demas estados que no se pueden reseñar en la lista. Esta vez en el endpoint para reseñar

--- src/main/java/com/carpool/carpool/service/review/ReviewImplementation.java
+++ src/main/java/com/carpool/carpool/service/review/ReviewImplementation.java
@@ -102,7 +102,13 @@ public class ReviewImplementation implements IReviewService {
         }
 
         //Verificar que el id del targetUser que se envia tenga una reserva asociada a dicho viaje
-        List<String> excludedStates = List.of(ReservationStateEnum.REJECTED.name(), ReservationStateEnum.CANCELLED.name());
+        List<String> excludedStates = List.of(
+                ReservationStateEnum.PENDING.name(),
+                ReservationStateEnum.ACCEPTED.name(),
+                ReservationStateEnum.IN_PROGRESS.name(),
+                ReservationStateEnum.REJECTED.name(),
+                ReservationStateEnum.CANCELLED.name()
+        );
 
         reservationRepository.findReservationByUserAndTripExcludingStates(targetUser.getId(), trip.getId(), excludedStates)
                 .orElseThrow(() -> {
