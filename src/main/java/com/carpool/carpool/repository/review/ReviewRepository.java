fa4474f761badc57a66d09f4c37e016b233b34de
Roberto Salera <robertosalera15@gmail.com>
2026-04-11 14:47:31 -0300

    [FIX]: Correccion para que cuando se consulten las reservas de un chofer devuelva solo las que le hicieron como chofer

--- src/main/java/com/carpool/carpool/repository/review/ReviewRepository.java
+++ src/main/java/com/carpool/carpool/repository/review/ReviewRepository.java
@@ -3,11 +3,9 @@ package com.carpool.carpool.repository.review;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,6 +22,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         FROM Review r
         WHERE r.targetUser.id = :userId
         AND r.deletedAt IS NULL
+        AND r.passengerToDriver = true
     """)
     List<Review> findReviewsByTargetUser(
         @Param("userId") Long userId,
