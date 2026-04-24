d5218aace6b065b8432bc5064efd0325a23b2a1b
gonzalo-baldassi <gonzalobaldassi02@gmail.com>
2026-04-24 19:31:27 -0300

    [FEAT]: Endpoint para desactivar las notificaciones push y para saber el estado de las mismas, si estan activas o no

--- src/main/java/com/carpool/carpool/repository/user/token/UserTokenRepository.java
+++ src/main/java/com/carpool/carpool/repository/user/token/UserTokenRepository.java
@@ -5,6 +5,8 @@ import com.carpool.carpool.enums.token.TokenTypeEnum;
 import com.carpool.carpool.model.user.User;
 import com.carpool.carpool.model.user.token.UserToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +15,8 @@ import java.util.Optional;
 public interface UserTokenRepository extends JpaRepository<UserToken,Long> {
     Optional<UserToken> findByToken(String token);
     List<UserToken> findByUserAndTypeAndState(User user, TokenTypeEnum type, TokenStateEnum state);
+
+    @Transactional
+    @Modifying
+    void deleteByUserIdAndType(Long userId, TokenTypeEnum type);
 }
