5500d2676d2a034fa5ab37c69771aa533c86392a
SGodoy21 <santygodoy64@gmail.com>
2026-04-29 18:41:06 -0300

    [FIX]: Cambios en la query para traer traer los nuevos usuarios agrupados por el group By

--- src/main/java/com/carpool/carpool/service/statistics/admin/user/AdminUserStatsImplementation.java
+++ src/main/java/com/carpool/carpool/service/statistics/admin/user/AdminUserStatsImplementation.java
@@ -50,8 +50,16 @@ public class AdminUserStatsImplementation implements IAdminUserStatsService {
     public Response<AdminStatSimpleDTO> getNewUsersStats(LocalDate fromDate, LocalDate toDate, GroupByEnum groupBy) {
         validateDate(fromDate, toDate); 
 
-        LocalDateTime fromDateTime = fromDate != null ? fromDate.atStartOfDay() : null;
-        LocalDateTime toDateTime   = toDate != null ? toDate.atTime(23, 59, 59) : null;
+       LocalDateTime fromDateTime;
+        LocalDateTime toDateTime;
+
+        if (groupBy == GroupByEnum.WEEK) {
+            fromDateTime = LocalDateTime.now().minusDays(7);
+            toDateTime   = LocalDateTime.now();
+        } else {
+            fromDateTime = fromDate != null ? fromDate.atStartOfDay() : null;
+            toDateTime   = toDate   != null ? toDate.atTime(23, 59, 59) : null;
+        }
 
         double historicalTotal = Optional.ofNullable(
             adminUserStatisticsRepository.countNewUsersByDateRange(null, null)
