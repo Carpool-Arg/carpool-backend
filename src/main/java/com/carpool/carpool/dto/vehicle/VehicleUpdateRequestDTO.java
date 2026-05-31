62802481a0d322157ed0ed631dff3a2f229cd1ce
Roberto Salera <robertosalera15@gmail.com>
2026-05-31 19:16:07 -0300

    [FIX]: Correccion de la reuqest para actualizar el vehiculo

--- src/main/java/com/carpool/carpool/dto/vehicle/VehicleUpdateRequestDTO.java
+++ src/main/java/com/carpool/carpool/dto/vehicle/VehicleUpdateRequestDTO.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class VehicleUpdateRequestDTO {
     @NotBlank(message = "La marca del vehiculo no puede estar en blanco.")
-    @Pattern(regexp = "^[A-Za-z-]+$", message = "La marca debe contener sólo letras, números y espacios.")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "La marca debe contener sólo letras, números y espacios.")
     private String brand;
 
     @NotBlank(message = "El modelo del vehiculo no puede estar en blanco.")
